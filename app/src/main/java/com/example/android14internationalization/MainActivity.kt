27fb3894df74a756a61ce7ab66d715fa43d4ad30
mdupierreux <maxime.dupierreux@gmail.com>
2023-02-12 21:46:39 +0100

    feat: go to settings

--- app/src/main/java/com/example/android14internationalization/MainActivity.kt
+++ app/src/main/java/com/example/android14internationalization/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.android14internationalization
 
 import android.app.LocaleConfig
 import android.app.LocaleManager
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.LocaleList
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -21,9 +24,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
 import com.example.android14internationalization.ui.theme.Android14InternationalizationTheme
 import java.util.Locale
+
 
 class MainActivity : ComponentActivity() {
 
@@ -31,13 +36,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val context = LocalContext.current
             Android14InternationalizationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ChangeLanguageScreen()
+                    ChangeLanguageScreen(
+                        Modifier.padding(16.dp)
+                    ) {
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                        val uri = Uri.fromParts("package", packageName, null)
+                        intent.data = uri
+                        ContextCompat.startActivity(context, intent, null)
+                    }
                 }
             }
         }
@@ -45,9 +58,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ChangeLanguageScreen(modifier: Modifier = Modifier) {
+fun ChangeLanguageScreen(
+    modifier: Modifier = Modifier,
+    onGoToSettingsClicked: () -> Unit
+) {
     val localeManager = getSystemService(LocalContext.current, LocaleManager::class.java)
-    val localeConfig = LocaleConfig(LocaleList(Locale.FRENCH))
+    val localeConfig = LocaleConfig(LocaleList(Locale.FRENCH, Locale.GERMAN, Locale.ENGLISH))
     Column(
         modifier,
         Arrangement.Center,
@@ -55,14 +71,16 @@ fun ChangeLanguageScreen(modifier: Modifier = Modifier) {
     ) {
         Text(
             text = stringResource(id = R.string.hello),
-            modifier = Modifier.padding(16.dp)
         )
         Button(
-            modifier = Modifier.height(56.dp),
-            onClick = {
-                localeManager?.overrideLocaleConfig = localeConfig
-            }) {
-            Text(text = "Change languages")
+            onClick = { localeManager?.overrideLocaleConfig = localeConfig }
+        ) {
+            Text(text = stringResource(R.string.load_languages))
+        }
+        Button(
+            onClick = { onGoToSettingsClicked() }
+        ) {
+            Text(text = stringResource(R.string.go_to_settings))
         }
     }
 }
